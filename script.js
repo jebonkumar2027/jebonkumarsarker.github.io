3e48f50d587b5b2a1dab2ed369e693960e3a422c
Jebon Kumar Sarker <jibontalk2020@gmail.com>
2026-09-23 09:59:00 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const menuToggle = document.getElementById("menu-toggle");
 
+const navMenu = document.getElementById("nav-menu");
+
+
+menuToggle.addEventListener("click", function () {
+
+    navMenu.classList.toggle("active");
+
+});
+
+
+const navLinks = document.querySelectorAll("#nav-menu a");
+
+
+navLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navMenu.classList.remove("active");
+
+    });
+
+});
